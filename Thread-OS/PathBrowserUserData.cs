8581e2883735540a94bd7867f4c29a7e852a1f01
atom20000 <0362725@mail.ru>
2021-02-13 22:13:23 +0300

    Pair Yandex.browser and ChromeWebDrover

---
+++ Thread-OS/PathBrowserUserData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Thread_OS
+{
+    class PathBrowserUserData
+    {
+        public string BrowserPath;
+        public string User_DataPath;
+        /// <summary>
+        /// Создает объект из Json-файла с названием name 
+        /// </summary>
+        /// <param name="name"></param>
+        public PathBrowserUserData(string name)
+        {
+            string FileText = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), name));
+            this.BrowserPath = JsonConvert.DeserializeObject<PathBrowserUserData>(FileText).BrowserPath;
+            this.User_DataPath = JsonConvert.DeserializeObject<PathBrowserUserData>(FileText).User_DataPath;
+        }
+        public PathBrowserUserData() { }
+        /// <summary>
+        /// Сохраняет объект в Json-файл с названием name 
+        /// </summary>
+        /// <param name="name"></param>
+        public void ToJsonFile(string name)=>
+            File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), name), JsonConvert.SerializeObject(this, Formatting.Indented));
+    }
+}
---
+++ Thread-OS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace Thread_OS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PathBrowserUserData pathBrowserUserData = new PathBrowserUserData("config.json");
+            ChromeOptions options = new ChromeOptions
+            {
+                BinaryLocation = pathBrowserUserData.BrowserPath
+            };
+            options.AddArgument(pathBrowserUserData.User_DataPath);
+            ChromeDriver chromeDriver = new ChromeDriver(options);
+            chromeDriver.Navigate().GoToUrl("https://vk.com/feed");
+        }
+    }
+}
