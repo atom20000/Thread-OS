1906000b70be08013643e65cd72dd94a4713419a
atom20000 <0362725@mail.ru>
2021-04-27 20:40:23 +0300

    Delete DMA project and create new DMA project. Write base part code, but check all again

--- DMA/Program.cs
+++ DMA/Program.cs
@@ -1,12 +1,103 @@
 ﻿using System;
+using System.IO;
+using System.Dynamic;
+using System.Collections.Generic;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
 
 namespace DMA
 {
     class Program
     {
+        static readonly string File_path="DMA.json";
+        static readonly string Mutex_name = "DMA_file";
+        static readonly string[] eventwaithandle_name = new string[]
+        {
+            "DMA",
+            "Pepheral device"
+        };
+        static public EventWaitHandle[] eventWaitHandle = new EventWaitHandle[2];
         static void Main(string[] args)
         {
-            Сentral_Processor.Create_file().Start();
+            for (int i = -1; Menu(i); i++);
+        }
+        static bool Menu(int Id_process)
+        {
+            Console.WriteLine("1. Клавиатура \n2. Принтер \n3.Выход");
+            int way;
+            if (!int.TryParse(Console.ReadLine(), out way))
+                return true;
+            switch (way)
+            {
+                case (1):
+                    Keyboard(Id_process);
+                    break;
+                case (2):
+                    Printer(Id_process);
+                    break;
+                case (3):
+                    return false;
+
+            }
+            return true;
+        }
+        static void Keyboard(object Id_process)
+        {
+            //Добавить мьютексы
+            Mutex mutex = new Mutex(false, Mutex_name);
+            CheckorCreateEventWaitHandle();
+            eventWaitHandle[0].WaitOne();
+            mutex.WaitOne();
+            var table = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText(File_path));
+            table.Add( new JObject
+            {
+                {"Process_id", (int)Id_process},
+                {"Code_operation", 0},
+                {"Result", null}
+            }
+            );
+            File.WriteAllText(File_path, JsonConvert.SerializeObject(table, Formatting.Indented));
+            mutex.ReleaseMutex();
+            eventWaitHandle[0].Reset();
+            eventWaitHandle[1].Set();
+            eventWaitHandle[0].WaitOne();
+            mutex.WaitOne();
+            table = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText(File_path));
+            //table.ToList().Find(el => el.Process_id.Equals(Id_process));
+            //Добавить ожидание выполнение завершения процесса и удаление записи из файла и вывод на экран результата
+        }
+        static void Printer(object Id_process)
+        {
+            //Добавить мьютексы
+            Mutex mutex = new Mutex(false, Mutex_name);
+            CheckorCreateEventWaitHandle();
+            eventWaitHandle[0].WaitOne();
+            mutex.WaitOne();
+            var table = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText(File_path));
+            table.Add(new JObject
+            {
+                {"Process_id", (int)Id_process},
+                {"Code_operation", 1},
+                {"Result", Console.ReadLine()}
+            }
+            );
+            File.WriteAllText(File_path, JsonConvert.SerializeObject(table, Formatting.Indented));
+            mutex.ReleaseMutex();
+            eventWaitHandle[0].Reset();
+            eventWaitHandle[1].Set();
+            eventWaitHandle[0].WaitOne();
+            mutex.WaitOne();
+            table = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText(File_path));
+            //Добавить ожидание выполнение завершения процесса и удаление записи из файла
+        }
+        private static void CheckorCreateEventWaitHandle()
+        {
+            if (!EventWaitHandle.TryOpenExisting(eventwaithandle_name[0], out eventWaitHandle[0]))
+                eventWaitHandle[0] = new EventWaitHandle(true, EventResetMode.ManualReset, eventwaithandle_name[0]);
+            if (!EventWaitHandle.TryOpenExisting(eventwaithandle_name[1], out eventWaitHandle[1]))
+                eventWaitHandle[1] = new EventWaitHandle(false, EventResetMode.ManualReset, eventwaithandle_name[1]);
         }
     }
 }
---
+++ peripheral device/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+
+namespace peripheral_device
+{
+    class Program
+    {
+        static readonly string File_path = "DMA.json";
+        static readonly string Mutex_name = "DMA_file";
+        static readonly string[] eventwaithandle_name = new string[]
+        {
+            "DMA",
+            "Pepheral device"
+        };
+        static public EventWaitHandle[] eventWaitHandle = new EventWaitHandle[2];
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                CheckorCreateEventWaitHandle();
+                eventWaitHandle[1].WaitOne();
+                Mutex mutex = new Mutex(false, Mutex_name);
+                mutex.WaitOne();
+                var table = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText(File_path));
+                foreach (var elem in table)
+                {
+                    switch (elem.Code_operation)
+                    {
+                        case (0):
+                            Console.WriteLine("Введите данные с клавиатуры");
+                            elem["Result"] = Console.ReadLine();
+                            break;
+                        case (1):
+                            Console.WriteLine($"Печать с принтера: {elem.Result}");
+                            break;
+                    }  
+                }
+                File.WriteAllText(File_path, JsonConvert.SerializeObject(table, Formatting.Indented));
+                mutex.ReleaseMutex();
+                eventWaitHandle[1].Reset();
+                eventWaitHandle[0].Set();
+            }
+        }
+        static void Keyboard()
+        {
+            //Добавить мьютексы
+            var table = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText(File_path));
+
+            File.WriteAllText(File_path, JsonConvert.SerializeObject(table, Formatting.Indented));
+            //Добавить ожидание выполнение завершения процесса и удаление записи из файла
+        }
+        static void Printer()
+        {
+            //Добавить мьютексы
+            var table = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText(File_path));
+
+            File.WriteAllText(File_path, JsonConvert.SerializeObject(table, Formatting.Indented));
+            //Добавить ожидание выполнение завершения процесса и удаление записи из файла
+        }
+        private static void CheckorCreateEventWaitHandle()
+        {
+            if (!EventWaitHandle.TryOpenExisting(eventwaithandle_name[0], out eventWaitHandle[0]))
+                eventWaitHandle[0] = new EventWaitHandle(false, EventResetMode.ManualReset, eventwaithandle_name[0]);
+            if (!EventWaitHandle.TryOpenExisting(eventwaithandle_name[1], out eventWaitHandle[1]))
+                eventWaitHandle[1] = new EventWaitHandle(true, EventResetMode.ManualReset, eventwaithandle_name[1]);
+        }
+    }
+}
